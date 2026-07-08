fbb731b06489c34f9dba2bbcb6f44e67c389232c
Kauri Kolehmainen <kauri.kolehmainen@helsinki.fi>
2026-07-08 15:53:17 +0300

    Added model for a note and made migrations

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.CharField(max_length=1000)
+    pub_date = models.DateTimeField("date published")
