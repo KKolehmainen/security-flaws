7895f082b8709e329b501dd7ae94120a0cfc90b8
Kauri Kolehmainen <kauri.kolehmainen@helsinki.fi>
2026-07-08 16:40:31 +0300

    Add index view and pages for notes

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Note
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_notes = Note.objects.order_by("-pub_date")[:5]
+    context = {
+        "latest_notes": latest_notes
+    }
+    return render(request, "notes/index.html", context)
