5a4ad6a4f7d5c56e116f1b733b5e2eabd7c7d0e2
Kauri Kolehmainen <kauri.kolehmainen@helsinki.fi>
2026-07-10 11:28:46 +0300

    Add user registration

--- notes/urls.py
+++ notes/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:note_id>/", views.noteView, name="note"),
+    path("register/", views.registerView, name="register"),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.views.decorators.csrf import csrf_exempt
 
 from .models import Note
 # Create your views here.
@@ -13,3 +16,26 @@ def index(request):
 def noteView(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
     return render(request, "notes/note.html", {"note": note})
+
+@csrf_exempt
+def registerView(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password1 = request.POST["password1"]
+        password2 = request.POST["password2"]
+
+        if password1 != password2:
+            messages.error(request, "Passwords do not match")
+            return redirect("/notes/register")
+        
+        if User.objects.filter(username=username).exists():
+            messages.error(request, "The username already exists")
+            return redirect("/notes/register")
+
+        user = User.objects.create_user(username, password=password1)
+        user.save()
+        return redirect("/notes/")
+    
+    if request.method == "GET":
+        return render(request, "notes/register.html")
+    
